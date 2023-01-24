e6ac367a92b78b1cd5864316292a0ab07364b1eb
andersson980114 <anderson.980114@gmail.com>
2023-01-24 18:16:35 -0500

    logica

--- main.js
+++ main.js
@@ -8,40 +8,35 @@ const app = new Vue({
         usuario:"",
         fechaNac:"",
         edad: 0,
-        clave:""
         
     },
-    methods:{
-        calcularEdad(){ 
-           
+    methods:{ 
+ 
+        agregarUsuario(){
             const hoy = new Date();
             this.edad = parseInt(hoy.getFullYear()) - parseInt(this.fechaNac.substring(0,4));
             const  month = parseInt(hoy.getMonth()) - parseInt(this.fechaNac.substring(5,7)) 
             const  day = parseInt(this.fechaNac.substring(8,10))
-           
+            const random = Math.floor(Math.random() * 9999);
             if (month < 0  && hoy.getDate() < day ) {
                 this.edad--;
-                
             }
-            
-        },
 
-        generarClave(){
-
-        },
-
-        agregarUsuario(){
-            usuarios.push({
+            //console.log(this.edad)
+            this.usuarios.push({
                 'nombres': this.nombres,
                 'apellidos': this.apellidos,
                 'nombreCompleto': this.nombres +" "+ this.apellidos,
+                'usuario': this.usuario,
                 'edad': this.edad,
-                'clave': this.clave
+                'clave': this.nombres.substring(0, 3) + this.apellidos.substring(0,3) + random
+                
             })
-
+ 
         },
         
     },
+ 
      
     
 })
